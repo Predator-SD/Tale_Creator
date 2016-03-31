a511863726cbf0b2dfaac72ce1d9a0cb2e3680e8
SD <zswdzly@outlook.com>
2016-03-31 12:44:38 +0800

    load scripts

--- An.py
+++ An.py
@@ -1,5 +1,15 @@
+import sys   
+sys.setrecursionlimit(100000000)
 class Tales(object):
-  def __init__(self,path):
-    self.path=path
-  def loadTale(self):
+  def __init__(self):
+    self.status={}
+    self.player="SD"
+    self.loadstatus()
+    self.name="Taylor"
+    self.loadscript()
+  def loadscript(self):
+    path='Data/Tale.json'
+    with open(path,'r') as f:
+      self.script=json.load(f)
+  def loadstatus(self):
     pass
